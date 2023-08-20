5debf707c21219bfd3e1ab2571e1572de25019c1
chenyang <chenyang@uzoo.cn>
2023-08-20 23:31:59 +0800

    add new tokenizer with fictions

--- config.py
+++ config.py
@@ -9,9 +9,13 @@ LANGUAGE = "enzh"  # [en, zh, enzh]
 # TOKENIZER_BIN = "tokenizers/baichuan/tokenizer.bin" # binary version of the tokenizer for inference in C
 
 # https://huggingface.co/ziqingyang/chinese-llama-2-7b/blob/main/tokenizer.model
-TOKENIZER_MODEL = "tokenizers/llama2enzh/tokenizer.model" # the llama2.
-TOKENIZER_BIN = "tokenizers/llama2enzh/tokenizer.bin" # binary version of the tokenizer for inference in C
+# TOKENIZER_MODEL = "tokenizers/llama2enzh/tokenizer.model" # the llama2.
+# TOKENIZER_BIN = "tokenizers/llama2enzh/tokenizer.bin" # binary version of the tokenizer for inference in C
 
 # base llama2，
 # TOKENIZER_MODEL = "tokenizers/llama2en/tokenizer.model" # the llama2-enzh.
 # TOKENIZER_BIN = "tokenizers/llama2en/tokenizer.bin" # binary version of the tokenizer for inference in C
+
+#自定义中文词表(红楼梦.txt)
+TOKENIZER_MODEL = "tokenizers/custom_tokenizer/meng.model" # the llama2-zh.
+TOKENIZER_BIN = "tokenizers/custom_tokenizer/meng.bin" # binary version of the tokenizer for inference in C
